dc22b17b041dab943db6e5d0b41210620a02de20
lomirus <lomirus@qq.com>
2022-05-30 21:02:18 +0800

    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Text Editor");
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+    }
+}
