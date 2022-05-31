3c4db190d33d759287c93a65a5757e51b05a1f8f
lomirus <lomirus@qq.com>
2022-05-31 21:56:34 +0800

    拆分了文件目录

---
+++ text_editor/FileMenu.java
@@ -0,0 +1,121 @@
+package text_editor;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFileChooser;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+public class FileMenu extends JMenu {
+    public FileMenu(App app) {
+        this.setText("文件");
+        JMenuItem newFile = new JMenuItem();
+        AbstractAction newFileAction = new AbstractAction("新建") {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("新建");
+            }
+        };
+        newFileAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
+        newFile.setAction(newFileAction);
+        this.add(newFile);
+
+        JMenuItem newWindow = new JMenuItem();
+        AbstractAction newWindowAction = new AbstractAction("新窗口") {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("新窗口");
+            }
+        };
+        newWindowAction.putValue(Action.ACCELERATOR_KEY,
+                KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK | KeyEvent.SHIFT_DOWN_MASK));
+        newWindow.setAction(newWindowAction);
+        this.add(newWindow);
+
+        JMenuItem open = new JMenuItem();
+        AbstractAction openAction = new AbstractAction("打开") {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                final String cwd = System.getProperty("user.dir");
+                final JFileChooser fileChooser = new JFileChooser(cwd);
+                fileChooser.showOpenDialog(app.frame);
+                final File file = fileChooser.getSelectedFile();
+                
+                try {
+                    String content = new String(Files.readAllBytes(file.toPath()));
+                    app.setContent(content);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        openAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
+        open.setAction(openAction);
+        this.add(open);
+
+        JMenuItem save = new JMenuItem();
+        AbstractAction saveAction = new AbstractAction("保存") {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("保存");
+            }
+        };
+        saveAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
+        save.setAction(saveAction);
+        this.add(save);
+
+        JMenuItem saveAs = new JMenuItem();
+        AbstractAction saveAsAction = new AbstractAction("另存为") {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("另存为");
+            }
+        };
+        saveAsAction.putValue(Action.ACCELERATOR_KEY,
+                KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK | KeyEvent.SHIFT_DOWN_MASK));
+        saveAs.setAction(saveAsAction);
+        this.add(saveAs);
+
+        this.addSeparator();
+
+        JMenuItem pageConfig = new JMenuItem();
+        AbstractAction pageConfigAction = new AbstractAction("页面设置") {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("页面设置");
+            }
+        };
+        pageConfig.setAction(pageConfigAction);
+        this.add(pageConfig);
+
+        JMenuItem print = new JMenuItem();
+        AbstractAction printAction = new AbstractAction("打印") {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("打印");
+            }
+        };
+        printAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_P, KeyEvent.CTRL_DOWN_MASK));
+        print.setAction(printAction);
+        this.add(print);
+
+        this.addSeparator();
+
+        JMenuItem exit = new JMenuItem();
+        AbstractAction exitAction = new AbstractAction("退出") {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("退出");
+            }
+        };
+        exit.setAction(exitAction);
+        this.add(exit);
+    }
+}
--- text_editor/MenuBar.java
+++ text_editor/MenuBar.java
@@ -2,123 +2,17 @@ package text_editor;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
-import javax.swing.JFileChooser;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
 
 public class MenuBar extends JMenuBar {
-    public MenuBar(App parent) {
-        JMenu file = new JMenu("文件");
-
-        JMenuItem newFile = new JMenuItem();
-        AbstractAction newFileAction = new AbstractAction("新建") {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                System.out.println("新建");
-            }
-        };
-        newFileAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
-        newFile.setAction(newFileAction);
-        file.add(newFile);
-
-        JMenuItem newWindow = new JMenuItem();
-        AbstractAction newWindowAction = new AbstractAction("新窗口") {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                System.out.println("新窗口");
-            }
-        };
-        newWindowAction.putValue(Action.ACCELERATOR_KEY,
-                KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK | KeyEvent.SHIFT_DOWN_MASK));
-        newWindow.setAction(newWindowAction);
-        file.add(newWindow);
-
-        JMenuItem open = new JMenuItem();
-        AbstractAction openAction = new AbstractAction("打开") {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                final String cwd = System.getProperty("user.dir");
-                final JFileChooser fileChooser = new JFileChooser(cwd);
-                fileChooser.showOpenDialog(parent.frame);
-                final File file = fileChooser.getSelectedFile();
-                
-                try {
-                    String content = new String(Files.readAllBytes(file.toPath()));
-                    parent.setContent(content);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        openAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
-        open.setAction(openAction);
-        file.add(open);
-
-        JMenuItem save = new JMenuItem();
-        AbstractAction saveAction = new AbstractAction("保存") {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                System.out.println("保存");
-            }
-        };
-        saveAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
-        save.setAction(saveAction);
-        file.add(save);
-
-        JMenuItem saveAs = new JMenuItem();
-        AbstractAction saveAsAction = new AbstractAction("另存为") {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                System.out.println("另存为");
-            }
-        };
-        saveAsAction.putValue(Action.ACCELERATOR_KEY,
-                KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK | KeyEvent.SHIFT_DOWN_MASK));
-        saveAs.setAction(saveAsAction);
-        file.add(saveAs);
-
-        file.addSeparator();
-
-        JMenuItem pageConfig = new JMenuItem();
-        AbstractAction pageConfigAction = new AbstractAction("页面设置") {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                System.out.println("页面设置");
-            }
-        };
-        pageConfig.setAction(pageConfigAction);
-        file.add(pageConfig);
-
-        JMenuItem print = new JMenuItem();
-        AbstractAction printAction = new AbstractAction("打印") {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                System.out.println("打印");
-            }
-        };
-        printAction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_P, KeyEvent.CTRL_DOWN_MASK));
-        print.setAction(printAction);
-        file.add(print);
-
-        file.addSeparator();
-
-        JMenuItem exit = new JMenuItem();
-        AbstractAction exitAction = new AbstractAction("退出") {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                System.out.println("退出");
-            }
-        };
-        exit.setAction(exitAction);
-        file.add(exit);
+    public MenuBar(App app) {
+        JMenu file = new FileMenu(app);        
 
         JMenu edit = new JMenu("编辑");
 
